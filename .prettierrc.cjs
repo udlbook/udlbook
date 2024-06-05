def3e5234b67d7e3687c3b91fe22a94cfee344a4
Tom Heaton <thomas.m.heaton@gmail.com>
2024-06-05 23:56:37 +0100

    setup formatting

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+    trailingComma: "all",
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    printWidth: 100,
+    endOfLine: "lf",
+    plugins: [
+        require.resolve("prettier-plugin-organize-imports"),
+    ],
+};
+
+module.exports = prettierConfig;
