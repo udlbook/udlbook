6503c85886928a1a65d3bfcdc14d63edc44bea57
Tom Heaton <thomas.m.heaton@gmail.com>
2024-06-19 14:32:36 +0100

    fix navbar naming

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import InstructorsSection from "@/components/Instructors";
 import MediaSection from "@/components/Media";
 import MoreSection from "@/components/More";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 import NotebookSection from "@/components/Notebooks";
 import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
